76965f8678303feb4abe5d22aced8dae586f6211
fallintoplace <38443830+fallintoplace@users.noreply.github.com>
2020-07-14 21:54:51 +0200

    Update satisfaction_prediction.py

--- satisfaction_prediction.py
+++ satisfaction_prediction.py
@@ -29,9 +29,9 @@ dataset = pd.read_csv(TRAIN_URL)
 dataset = dataset.dropna()
 dataset.pop(KEY)
 
-dataset = dataset.sample(frac=1).reset_index(drop=True)
+dataset = dataset.sample(frac = 1).reset_index(drop = True)
 
-train_dataset = dataset.sample(frac=1,random_state=0)
+train_dataset = dataset.sample(frac = 1,random_state = 0)
 test_dataset = dataset.drop(train_dataset.index)
 
 train_labels = train_dataset.pop(TARGET)
@@ -53,42 +53,37 @@ train_dataset = norm(train_dataset)
 print(train_dataset.head())
 
 """
-TWO DENSELY PRELU ACTIVATED CONNECTED LAYERS WITH DROPOUTS, REGULARIZATIONS
-AND LAYER NORMALIZATIONS
+TWO DENSELY PRELU ACTIVATED CONNECTED LAYERS WITH DROPOUTS, AND LAYER NORMALIZATIONS
 
 """
 
 def build_model():
     model = Sequential([
-        layers.Dense(
-            50, input_shape=(test_dataset.shape[-1],),
-        ),
+        layers.Dense(50, input_shape = (test_dataset.shape[-1],),),
+        layers.PReLU(alpha_initializer = tf.initializers.constant(0.25)),
+        layers.LayerNormalization(),
+        layers.Dropout(0.5),
+        layers.Dense(50), 
         layers.PReLU(alpha_initializer=tf.initializers.constant(0.25)),
         layers.LayerNormalization(),
         layers.Dropout(0.5),
-        layers.Dense(50),
-        layers.PReLU(alpha_initializer=tf.initializers.constant(0.25)),
-        layers.LayerNormalization(),
-        layers.Dropout(0.5),
-        layers.Dense(1, activation="sigmoid"),
+        layers.Dense(1, activation = "sigmoid"),
         ])
 
     metrics = [
-        keras.metrics.FalseNegatives(name="fn"),
-        keras.metrics.FalsePositives(name="fp"),
-        keras.metrics.TrueNegatives(name="tn"),
-        keras.metrics.TruePositives(name="tp"),
-        keras.metrics.Precision(name="precision"),
-        keras.metrics.Recall(name="recall"),
-        keras.metrics.AUC(name='auc'),
+        keras.metrics.FalseNegatives(name = "fn"),
+        keras.metrics.FalsePositives(name = "fp"),
+        keras.metrics.TrueNegatives(name = "tn"),
+        keras.metrics.TruePositives(name = "tp"),
+        keras.metrics.Precision(name = "precision"),
+        keras.metrics.Recall(name = "recall"),
+        keras.metrics.AUC(name = 'auc'),
     ]
     
     model.compile(
-        optimizer=keras.optimizers.Adam(0.0005), loss="binary_crossentropy", metrics=metrics
+        optimizer = keras.optimizers.Adam(0.001), loss = "binary_crossentropy", metrics = metrics
     )
     
-
-
     return model
 
 model = build_model()
@@ -100,12 +95,6 @@ FITTING AND PLOTTING THE LOSS VALUE GRAPH
 """
 
 counts = np.bincount(train_labels)
-print(
-    "Number of positive samples in training data: {} ({:.2f}% of total)".format(
-        counts[1], 100 * float(counts[1]) / len(train_labels)
-    )
-)
-
 weight_for_0 = 1.0 / counts[0]
 weight_for_1 = 1.0 / counts[1]
 
@@ -115,18 +104,19 @@ class_weight = {0: weight_for_0, 1: weight_for_1}
 history = model.fit(
     train_dataset,
     train_labels,
-    batch_size=256,
-    epochs=300,
-    verbose=1,
-    callbacks = [keras.callbacks.EarlyStopping(monitor = 'val_auc', patience = 10)],
+    batch_size = 256,
+    epochs = 300,
+    verbose = 1,
+    callbacks = [keras.callbacks.EarlyStopping(monitor = 'val_precision', patience = 30)],
     validation_split = 0.2,
-    class_weight=class_weight,
+    class_weight = class_weight,
 )
 plt.ylim([0, 1])
 plt.plot(history.history["auc"], label="Training AUC")
 plt.plot(history.history["val_auc"], label="Validation AUC")
 plt.legend()
 plt.show()
+
 
 
 """
@@ -143,6 +133,6 @@ PRINTING THE RESULT
 
 result = pd.DataFrame()
 result[KEY] = final_key
-result[TARGET]=model.predict(final).flatten()
-result[TARGET]=result[TARGET].apply(lambda x: 0 if (x<0.5) else 1)
+result[TARGET] = model.predict(final).flatten()
+result[TARGET] = result[TARGET].apply(lambda x: 0 if (x<0.5) else 1)
 pd.DataFrame(result).to_csv(RESULT_URL, index = False)
